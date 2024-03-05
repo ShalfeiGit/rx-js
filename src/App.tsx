59c550801126df980efa9742319a7d87c3ac2bff
Valentin Zadorozhniy <Zadorozhnaya@mail.ru>
2024-03-05 12:18:03 +0700

    add custom observables

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,45 @@
-function App() {
+import React from "react";
+import СomparisonApproach from "./components/1-СomparisonApproach"
+import Observables from "./components/2-Observables"
+import { Tree, TreeDataNode } from "antd";
+import {
+  DownOutlined,
+} from '@ant-design/icons';
+import './app.scss'
+
+const App: React.FC = () =>  {
+  const treeData: TreeDataNode[] = [
+    { 
+      title: 'Сomparison Approach', 
+      key: '0',
+      children: [
+        {
+          title: '',
+          key: '0-1',
+          icon: <СomparisonApproach />,
+        },
+      ],
+    },
+    { 
+      title: 'Observables', 
+      key: '1',
+      children: [
+        {
+          title: '',
+          key: '1-1',
+          icon: <Observables />,
+        },
+      ],
+    },
+  ];
 
   return (
     <>
-      Hello world!
+      <Tree
+        showIcon
+        switcherIcon={<DownOutlined />}
+        treeData={treeData}
+      />
     </>
   )
 }
